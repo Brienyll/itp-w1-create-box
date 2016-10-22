263385b954272bcf509e82ef5144c34a8f013493
Brienyll <brienyll29@gmail.com>
2016-10-22 09:15:01 -0700

    Update main.py

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,11 @@
 
 
 def create_box(height, width, character):
-    pass
-
-
+    if height >= 1:
+        i = width * character
+        j = 0
+        while j in range(height):
+            print(i)
+            j = j +1
 if __name__ == '__main__':
     create_box(3, 4, '*')
